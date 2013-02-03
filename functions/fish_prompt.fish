c76fec7a2d28c0672263830a3d101ccb488a3814
Steven Yang <yangchenyun@gmail.com>
2013-02-03 22:38:36 +0800

    updated the color of the git branch in prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -48,7 +48,9 @@ function fish_prompt --description 'Write out the prompt'
 		set -g __fish_prompt_host (set_color $fish_color_host)
 	end
 
-	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" 
+	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (pwd) 
+  echo -n -s (set_color brown) (__fish_git_prompt) (set_color normal)
+  echo -n -s "$__fish_prompt_normal" "$prompt_status" 
   echo
   echo "$delim" ''
 end
