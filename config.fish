f8c07a33116235d98605f991ee8d409e8cd2b107
Steven Yang <yangchenyun@gmail.com>
2013-02-03 22:24:58 +0800

    correct export env variables into fish

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
 # need a way to auto load these
+. "$HOME/.config/fish/functions/export.fish"
 . "$HOME/.config/fish/functions/aliases.fish"
 . "$HOME/.config/fish/functions/utils.fish"
--- functions/export.fish
+++ functions/export.fish
@@ -2,6 +2,8 @@
 set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 
+set -x PATH "$HOME/bin" $PATH
+
 # add homebrew to the beginning of PATH
 set -x PATH "/usr/local/bin/" $PATH
 set -x PATH "/usr/local/sbin/" $PATH
@@ -20,7 +22,7 @@ set -x NODE_PATH "/usr/local/lib/node_modules"
 set -x PATH "$HOME/.cabal/bin" $PATH
 
 # android SDK
-set -x ANDROID_SDK_ROOT=/usr/local/Cellar/android-sdk/r21
+set -x ANDROID_SDK_ROOT /usr/local/Cellar/android-sdk/r21
 
 # LESS with colors
 # from http://blog.0x1fff.com/2009/11/linux-tip-color-enabled-pager-less.html
@@ -41,8 +43,8 @@ setenv -x GREP_OPTIONS "--color=auto"
 
 # to make Ruby faster
 # https://gist.github.com/2113359
-set -x RUBY_HEAP_MIN_SLOTS=1000000
-set -x RUBY_HEAP_SLOTS_INCREMENT=1000000
-set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-set -x RUBY_GC_MALLOC_LIMIT=1000000000
-set -x RUBY_HEAP_FREE_MIN=500000
+set -x RUBY_HEAP_MIN_SLOTS 1000000
+set -x RUBY_HEAP_SLOTS_INCREMENT 1000000
+set -x RUBY_HEAP_SLOTS_GROWTH_FACTOR 1
+set -x RUBY_GC_MALLOC_LIMIT 1000000000
+set -x RUBY_HEAP_FREE_MIN 500000
