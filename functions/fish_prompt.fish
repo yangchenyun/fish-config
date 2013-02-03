90d7b06d9b924f16104bea96d9dcf7565a2b57f8
Steven Yang <yangchenyun@gmail.com>
2013-02-03 18:29:46 +0800

    configured the prompt

---
+++ functions/fish_prompt.fish
@@ -0,0 +1,54 @@
+function fish_prompt --description 'Write out the prompt'
+	
+	set -l last_status $status
+
+	# Just calculate these once, to save a few cycles when displaying the prompt
+	if not set -q __fish_prompt_hostname
+		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+	end
+
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
+
+	set -l delim '>'
+
+	switch $USER
+
+	case root
+
+		if not set -q __fish_prompt_cwd
+			if set -q fish_color_cwd_root
+				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+			else
+				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+			end
+		end
+
+	case '*'
+
+		if not set -q __fish_prompt_cwd
+			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+		end
+
+	end
+
+	set -l prompt_status
+	if test $last_status -ne 0
+		if not set -q __fish_prompt_status
+			set -g __fish_prompt_status (set_color $fish_color_status)
+		end
+		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+	end
+
+	if not set -q __fish_prompt_user
+		set -g __fish_prompt_user (set_color $fish_color_user)
+	end
+	if not set -q __fish_prompt_host
+		set -g __fish_prompt_host (set_color $fish_color_host)
+	end
+
+	echo -n -s "$__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" 
+  echo
+  echo "$delim" ''
+end
