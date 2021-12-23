d83b9ce9df46c09e44cdbc469a901f3c4a2ca434
Steven Yang <yangchenyun@gmail.com>
2021-12-24 02:08:28 +0800

    indent fix

--- functions/utils.fish
+++ functions/utils.fish
@@ -11,18 +11,18 @@ alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 function server --description "Start an HTTP server from a directory"
-  open http://localhost:8080/ 
-  and python -m SimpleHTTPServer 8080
-end 
+    open http://localhost:8080/
+    and python -m SimpleHTTPServer 8080
+end
 
 # function httpcompression
-  # curl -LIs -H 'User-Agent: Mozilla/5 Gecko' -H 'Accept-Encoding: gzip,deflate,compress,sdch' $argv | grep '^Content-Encoding:'
-  # and echo "$argv is encoded using ${encoding#* }" 
-  # or echo "$argv is not using any encoding"
+# curl -LIs -H 'User-Agent: Mozilla/5 Gecko' -H 'Accept-Encoding: gzip,deflate,compress,sdch' $argv | grep '^Content-Encoding:'
+# and echo "$argv is encoded using ${encoding#* }"
+# or echo "$argv is not using any encoding"
 # end
 
 function digga --description "All the dig info"
-  dig +nocmd $argv[1] any +multiline +noall +answer
+    dig +nocmd $argv[1] any +multiline +noall +answer
 end
 
 alias myip="curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'"
@@ -40,37 +40,38 @@ alias fs="stat -f \"%z bytes\""
 alias rot13='tr a-zA-Z n-za-mN-ZA-M'
 
 function md
-  mkdir -p "$argv"; cd "$argv"
+    mkdir -p "$argv"
+    cd "$argv"
 end
 
 function imsg --description "send imessage to my wife"
-  osascript -e "tell application \"Messages\" to send \"$argv\" to buddy \"Yun Wu\""
+    osascript -e "tell application \"Messages\" to send \"$argv\" to buddy \"Yun Wu\""
 end
 
 function randpw --description "generate a random password"
-  dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | rev | cut -b 2- | rev
+    dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | rev | cut -b 2- | rev
 end
 
 function cd --description "auto ls for each cd"
-  if [ -n $argv[1] ]
-    builtin cd $argv[1]
-    and ls -AF
-  else
-    builtin cd ~
-    and ls -AF
-  end
+    if [ -n $argv[1] ]
+        builtin cd $argv[1]
+        and ls -AF
+    else
+        builtin cd ~
+        and ls -AF
+    end
 end
 
 function pkill --description "pkill a process interactively"
-  ps aux | peco | awk '{ print $2 }' | xargs kill
+    ps aux | peco | awk '{ print $2 }' | xargs kill
 end
 
 function ppkill --description "kill -9 a process interactively"
-  ps aux | peco | awk '{ print $2 }' | xargs kill -KILL
+    ps aux | peco | awk '{ print $2 }' | xargs kill -KILL
 end
 
 function pgrep --description "pgrep a process interactively"
-  ps aux | peco | awk '{ print $2 }'
+    ps aux | peco | awk '{ print $2 }'
 end
 
 function rename --description "Rename a symbol recursively in a folder"
@@ -82,7 +83,7 @@ end
 ################################
 
 function gpip
-  env PIP_REQUIRE_VIRTUALENV='' pip $argv
+    env PIP_REQUIRE_VIRTUALENV='' pip $argv
 end
 
 ################################
@@ -111,8 +112,8 @@ alias spotoff="sudo mdutil -a -i off"
 alias spoton="sudo mdutil -a -i on"
 
 function posix-source
-	for i in (cat $argv)
-		set arr (echo $i |tr = \n)
-  		set -gx $arr[1] $arr[2]
-	end
+    for i in (cat $argv)
+        set arr (echo $i |tr = \n)
+        set -gx $arr[1] $arr[2]
+    end
 end
