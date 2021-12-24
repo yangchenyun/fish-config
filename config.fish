a259a3cfc46b741d8dc5bd38b2d75046d632872f
Steven Yang <yangchenyun@gmail.com>
2021-12-25 00:22:37 +0800

    config clean-up

--- config.fish
+++ config.fish
@@ -5,22 +5,14 @@
 
 # virtualenv support in fish
 set -g VIRTUALFISH_COMPAT_ALIASES
-. "$HOME/.config/fish/plugins/virtualfish/virtual.fish"
-. "$HOME/.config/fish/plugins/virtualfish/auto_activation.fish"
-. "$HOME/.config/fish/plugins/virtualfish/global_requirements.fish"
-
-# nvm-wrapper
-. "$HOME/.config/fish/plugins/nvm-wrapper/nvm.fish"
 
 # fenv
 set fish_function_path $fish_function_path "$HOME/.config/fish/plugins/plugin-foreign-env/functions"
 
 # Nix
 if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-      fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 end
 
 # Shell Integration
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-posix-source "$HOME/.fetchenv"
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
