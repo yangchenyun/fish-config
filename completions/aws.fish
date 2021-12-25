18cdcf6e2368505af7dd8e4a85ca3a7742401344
Steven Yang <yangchenyun@gmail.com>
2021-12-25 13:15:29 +0800

    port over all completions

---
+++ completions/aws.fish
@@ -0,0 +1,2 @@
+# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
