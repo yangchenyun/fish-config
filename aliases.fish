74942229303284ae24441d484b978f3520b1ebf2
Steven Yang <yangchenyun@gmail.com>
2021-12-27 10:01:15 +0800

    asdf direnv support in fish

--- aliases.fish
+++ aliases.fish
@@ -21,6 +21,12 @@ if type -q navi
     alias n=navi-widget
 end
 
+# NOTE: https://github.com/asdf-community/asdf-direnv
+if type -q asdf -a asdf exec direnv 1>2 2>/dev/null
+    test $status -eq 0
+    alias direnv "asdf exec direnv"
+end
+
 # kitty
 if test "$TERM" = xterm-kitty
     alias icat="kitty +kitten icat"
